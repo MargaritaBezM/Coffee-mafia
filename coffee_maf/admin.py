1fcce6c7104b157851812db5e9933ee9742e8f21
MargaritaBezM <rita432199@gmail.com>
2025-03-15 11:27:18 +0300

    Первый коммит

---
+++ coffee_maf/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from coffee_maf.models import *
+
+# Register your models here.
+admin.site.register(NewsModel)
+admin.site.register(TypeProductModel)
+admin.site.register(ProductModel)
+admin.site.register(VacancyModel)
+admin.site.register(VacancyPeersonModel)
