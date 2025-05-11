c6c433283b5f567f91ca67ea9fec49be0b7ad2ef
MargaritaBezM <rita432199@gmail.com>
2025-05-11 09:19:20 +0300

    Прописала пути

---
+++ coffee_maf/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home_page, name='main'),
+    path('news.html/', views.news_list, name='news'),
+    path('products.html/', views.products_list, name='products'),
+    path('vacanse.html/', views.vacancies_list, name='vacanse'),
+    path('contacts.html/', views.contacts_page, name='contacts'),
+]
--- coffee_maf/views.py
+++ coffee_maf/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import NewsModel, ProductModel, VacancyModel
+def home_page(request):
+    return render(request, 'coffee_maf/main.html')
 
-# Create your views here.
+def news_list(request):
+    news = NewsModel.objects.all().order_by('-id')
+    return render(request, 'coffee_maf/news.html', {'news': news})
+
+def products_list(request):
+    products = ProductModel.objects.all()
+    return render(request, 'coffee_maf/products.html', {'products': products})
+
+def vacancies_list(request):
+    vacancies = VacancyModel.objects.all()
+    return render(request, 'coffee_maf/vacanse.html', {'vacancies': vacancies})
+
+def contacts_page(request):
+    return render(request, 'coffee_maf/contacts.html')
