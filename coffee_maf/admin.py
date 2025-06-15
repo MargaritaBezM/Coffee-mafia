7ca388761e7288baba46736306709605ac4293fc
MargaritaBezM <rita432199@gmail.com>
2025-06-15 16:45:33 +0300

    Перевод названий таблиц на русский

--- coffee_maf/admin.py
+++ coffee_maf/admin.py
@@ -8,10 +8,9 @@ AdminSite.site_title = "Кофе Мафия"
 AdminSite.index_title = "Панель управления"
 
 
-# Register your models here.
-admin.site.register(NewsModel)
-admin.site.register(TypeProductModel)
-admin.site.register(ProductModel)
-admin.site.register(VacancyModel)
-admin.site.register(VacancyPersonModel)
+admin.site.register(NewsModel, verbose_name="Новость", verbose_name_plural="Новости")
+admin.site.register(TypeProductModel, verbose_name="Тип товара", verbose_name_plural="Типы товаров")
+admin.site.register(ProductModel, verbose_name="Товар", verbose_name_plural="Товары")
+admin.site.register(VacancyModel, verbose_name="Вакансия", verbose_name_plural="Вакансии")
+admin.site.register(VacancyPersonModel, verbose_name="Отклик", verbose_name_plural="Отклики")
 
