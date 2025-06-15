e17ba49b85d964c6c215e5824d2c8f256b2a9a5f
MargaritaBezM <rita432199@gmail.com>
2025-06-15 16:32:11 +0300

    Перевод админ панеои на русский

--- coffee_maf/admin.py
+++ coffee_maf/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
 from coffee_maf.models import *
+from django.contrib.admin import AdminSite
+
+AdminSite.site_header = "Кофе Мафия - Администрирование"
+AdminSite.site_title = "Кофе Мафия"
+AdminSite.index_title = "Панель управления"
+
 
 # Register your models here.
 admin.site.register(NewsModel)
@@ -8,3 +14,4 @@ admin.site.register(TypeProductModel)
 admin.site.register(ProductModel)
 admin.site.register(VacancyModel)
 admin.site.register(VacancyPersonModel)
+
